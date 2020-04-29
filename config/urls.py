51bcf7b439564f4e6004a0191eb78559a3a19da0
oryon-dominik <oryon@cyberise.de>
2020-04-29 15:20:27 +0200

    fixed TITLE

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html')),
+    path('', TemplateView.as_view(template_name='base.html'), {"TITLE": settings.TITLE}),
     path('', include('apps.users.urls', namespace="users")),
     path('admin/', admin.site.urls),
 ]
